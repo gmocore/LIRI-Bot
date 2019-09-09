fd1adffbbc7d9a46d40b76eb9b76d3327a604c1e
gerritt <gmocore@gmail.com>
2019-09-09 17:37:29 -0600

    added spotify api call

--- liri.js
+++ liri.js
@@ -1,33 +1,40 @@
 const dotenv = require("dotenv").config();
 const keys = require("./keys.js");
-// const spotify = new Spotify(keys.spotify);
+const Spotify = require("node-spotify-api");
 const axios = require("axios");
 const moment = require("moment");
-const artist = process.argv[2]
+const spotify = new Spotify({
+  id: process.env.SPOTIFY_CLIENT_ID,
+  secret: process.env.SPOTIFY_CLIENT_SECRET
+});
+// const artist = process.argv[2]
+const artist = process.argv.slice(2).join(" ");
 
 // process.argv.slice(2).join('%20)
 
-
-axios
-  .get(
-    `https://rest.bandsintown.com/artists/${artist}/events?app_id=${process.env.BANDS_IN_TOWN_API_KEY}`
-  )
-  .then(response => {
-    response.data.forEach(item => {
-      console.log(`venue name: ${item.venue.name} city: ${item.venue.city} state: ${item.venue.region} time: ${moment(item.datetime).format('MM/DD/YYYY')}`)
-    })
-    // console.log(response.data);
-  })
-
-  .catch(err => {
-    console.log("Error: ", err.code);
-  });
-
 // axios
-//   .get("https://api.spotify.com/v1/search?q=tania%20bowra&type=artist")
+//   .get(
+//     `https://rest.bandsintown.com/artists/${artist}/events?app_id=${process.env.BANDS_IN_TOWN_API_KEY}`
+//   )
 //   .then(response => {
-//     console.log(response);
+//     response.data.forEach(item => {
+//       console.log(`venue name: ${item.venue.name} city: ${item.venue.city} state: ${item.venue.region} time: ${moment(item.datetime).format('MM/DD/YYYY')}`)
+//     })
+//     // console.log(response.data);
 //   })
 //   .catch(err => {
-//     console.log("error: ", err);
+//     console.log("Error: ", err.code);
 //   });
+
+spotify
+  .search({ type: "track", query: artist, limit: 5 })
+  .then(response => {
+    let data = response.tracks.items[0]
+    console.log(data.name)
+    console.log(data.artists[0].name);
+    console.log(data.album.name);
+    console.log(data.preview_url);
+  })
+  .catch(err => {
+    console.log("Error: ", err);
+  });
